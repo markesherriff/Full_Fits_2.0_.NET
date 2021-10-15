f23cac9b14ea683dbe015a1dbfa7a6b13c079e34
Mark Sherriff <63994744+markesherriff@users.noreply.github.com>
2021-10-15 13:00:31 -0400

    Add files via upload

---
+++ ClientController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FullFits_MarkSherriff.Models;
+
+namespace FullFits_MarkSherriff.Controllers
+{
+    public class ClientController : Controller
+    {
+        private FullFitsDatabaseEntity db = new FullFitsDatabaseEntity();
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            return View(db.Catalogs.ToList());
+        }
+        //POST: Add to Cart from Catalog Page
+
+        // GET: Cart
+
+        public ActionResult Cart()
+        {
+            return View(db.Catalogs.ToList());
+        }
+
+        public ActionResult Orders()
+        {
+            return View(db.Sales.ToList());
+        }
+
+        public ActionResult AccountPage()
+        {
+            return View();
+        }
+
+        public void logout()
+        {
+
+        }
+
+        public void googleLogin()
+        {
+            
+        }
+    }
+}
